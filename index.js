44e916afbdfd8ea054a4fdb94e6e63b5a72b20f2
Zacchaeus Israel <zacchaeusisrael@Dickernss-MacBook-Pro.local>
2025-04-02 19:33:35 -0400

    converted if statement to a switch case.

--- index.js
+++ index.js
@@ -12,43 +12,49 @@ for (let i = 0; i < drumButtons.length; i++) {
     // access the button
     let btn = this;
     let btnLetter = btn.innerHTML;
+    let letCol = btn.style.color;
     // console.log('this = ', btn.innerHTML)
 
-    if (btnLetter === "w") {
-      // play the sound
-      let audio = new Audio("./sounds/tom-1.mp3");
-      audio.play();
+    // set the color of the character to white
+    letCol = 'white';
+
+    let audio = new Audio();
+    switch(btnLetter){
+        case 'w':
+            audio.src = "./sounds/tom-1.mp3";
+            audio.play();
+            break;
+        case 'a':
+            audio.src = "./sounds/tom-2.mp3";
+            audio.play();
+            break;
+
+        case 's':
+            audio.src = "./sounds/tom-3.mp3";
+            audio.play();
+            break;
+
+        case 'd':
+            audio.src = "./sounds/tom-4.mp3";
+            audio.play();
+            break;
+
+        case 'j':
+            audio.src = "./sounds/snare.mp3";
+            audio.play();
+            break;
+
+        case 'k':
+            audio.src = "./sounds/crash.mp3";
+            audio.play();
+            break;
+
+        case 'l':
+            audio.src = "./sounds/kick-bass.mp3";
+            audio.play();
+            break;
     }
-    if (btnLetter === "a") {
-      // play the sound
-      let audio = new Audio("./sounds/tom-2.mp3");
-      audio.play();
-    }
-    if (btnLetter === "s") {
-      // play the sound
-      let audio = new Audio("./sounds/tom-3.mp3");
-      audio.play();
-    }
-    if (btnLetter === "d") {
-      // play the sound
-      let audio = new Audio("./sounds/tom-4.mp3");
-      audio.play();
-    }
-    if (btnLetter === "j") {
-      // play the sound
-      let audio = new Audio("./sounds/snare.mp3");
-      audio.play();
-    }
-    if (btnLetter === "k") {
-      // play the sound
-      let audio = new Audio("./sounds/crash.mp3");
-      audio.play();
-    }
-    if (btnLetter === "l") {
-      // play the sound
-      let audio = new Audio("./sounds/kick-bass.mp3");
-      audio.play();
-    }
+   
   });
 }
 
