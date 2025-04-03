6e8f77ddc9b7ddc665614edf748b29650a355189
Zacchaeus Israel <zacchaeusisrael@Dickernss-MacBook-Pro.local>
2025-04-02 23:41:36 -0400

    Implemented code that prevents the animate function from being called if a invalid key was clicked.

--- index.js
+++ index.js
@@ -8,8 +8,12 @@ for (let i = 0; i < drumButtons.length; i++) {
     // access the button
     let btn = this;
     let btnLetter = btn.innerHTML;
-    //plays the sound 
+    //plays the sound
     playSound(btnLetter);
+
+    console.log("btnLetter = ", btnLetter);
+    // animate the button once it has been clicked
+    animateButton(btnLetter);
   });
 }
 
@@ -19,7 +23,14 @@ addEventListener("keydown", function (event) {
 
   // retrieve the key that was pressed
   let keyPressed = event.key;
-  playSound(keyPressed);
+  // play the sound
+  let soundPlayed = playSound(keyPressed);
+
+  // no need to animate the button if a key that's not on one of the drum buttons was clicked
+  if (soundPlayed !== false) {
+    // animate the respective button after the key has been clicked
+    animateButton(keyPressed);
+  }
 });
 
 // plays sound based on the letter
@@ -61,5 +72,30 @@ function playSound(key) {
       break;
     default:
       console.log("A different key was pressed. key = ", key);
+      return false;
+  }
+  return true;
+}
+
+// animates a button
+function animateButton(key) {
+  // each of the drum buttons have a class with its respective key; pass that key as a class
+  let btnClicked = document.querySelector(`.${key}`);
+  console.log("btnClicked = ", btnClicked);
+  if (btnClicked === null) {
+    // a key that's not a part of the game was clicked.
+    console.log(`There is no HTML element with the class \'${key}\'`);
+    alert(
+      `There is no HTML element with the class \'${key}.\'\nPlease select another key.`
+    );
+  } else {
+    // add class (in styles.css) to the button
+    btnClicked.classList.add("pressed");
+
+    // wait 0.25 seconds
+    setTimeout(function () {
+      // then removed the "pressed" class so that the button goes back to its original state
+      btnClicked.classList.remove("pressed");
+    }, 25);
   }
 }
